838b5607f9c39a65bbf13dd8fe2139a4e9cd7db7
ColdenStorm <51495718+ColdenStorm@users.noreply.github.com>
2026-02-16 20:24:02 +0300

    Update update-test.user.js

--- update-test.user.js
+++ update-test.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SPD Update Test
 // @namespace    https://github.com/ColdenStorm
-// @version      1.0.0
+// @version      1.0.1
 // @description  Test script for update system
 // @match        *://*/*
 // @updateURL    https://raw.githubusercontent.com/ColdenStorm/spd-worker-helper/main/update-test.user.js
@@ -11,5 +11,6 @@
 
 (function () {
     console.log("SPD Update Test version 1.0.0 loaded");
-    alert("Test script version 1.0.0");
+    alert("Test script version 1.0.1");
+
 })();
